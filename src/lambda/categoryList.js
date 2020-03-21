cb02c97eadde01880b6f0c36605a32bf5cee1bd2
Peter Barry <peter@everve.com>
2020-03-21 15:15:09 +0000

    add static lists

---
+++ src/lambda/categoryList.js
@@ -0,0 +1,23 @@
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  
+  try {
+return {
+      statusCode: 200,
+      body: JSON.stringify({categories: [
+        'Grocery', 
+        'Supermarket', 
+        'Farm', 
+        'Pharmacy',
+        'Sari-Sari',
+        'Cornershop'
+      ]})
+    }
+  } catch (err) {
+    console.log('get.categories', err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({msg: err.message})
+    }
+  }
+}
---
+++ src/lambda/productList.js
@@ -0,0 +1,27 @@
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  
+  try {
+return {
+      statusCode: 200,
+      body: JSON.stringify({products: [
+        'Toilet Paper/Roll', 
+        'Soap', 
+        'Wet wipes', 
+        'Disinfectant',
+        'Paracetomal',
+        'Lemons',
+        'Milk',
+        'Bread',
+        'Rice',
+        'Pasta',
+      ]})
+    }
+  } catch (err) {
+    console.log('product.list', err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({msg: err.message})
+    }
+  }
+}
