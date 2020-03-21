b31ee5bfdfed310876b31848d694c0c44cf89959
harry <harry@cell5.co.uk>
2020-03-21 18:12:52 +0000

    adding create shop form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import ProductTable from './components/ProductTable'
 import ShopForm from './components/ShopForm'
@@ -13,49 +12,16 @@ export default class App extends Component {
     return (
       <div className="App">
         <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/shop">shop</Link>
-                </li>
-                <li>
-                  <Link to="/users">Users</Link>
-                </li>
-              </ul>
-            </nav>
-
-            {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/shop">
-                <ShopForm />
-              </Route>
-              <Route path="/">
-                <ProductTable />
-              </Route>
-            </Switch>
-          </div>
+          <Switch>
+            <Route path="/shop">
+              <ShopForm />
+            </Route>
+            <Route path="/">
+              <ProductTable />
+            </Route>
+          </Switch>
         </Router>
       </div>
     );
   }
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
