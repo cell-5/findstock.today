f91d9f9e3db25edd8a1a871b1f4414368e23a0f3
Peter Barry <peter@everve.com>
2020-03-22 03:01:41 +0000

    First pass at screens

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
 import './App.css';
 
-import ProductTable from './components/ProductTable'
+import SearchPage from './components/search/SearchPage'
+import StockPage from './components/stock/StockPage'
+import ShopPage from './components/shop/ShopPage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
     render() {
       return (
       <div className="App">
-        <ProductTable />
+        <Router>
+          <Switch>
+            <Route path="/shop">
+              <ShopPage/>
+            </Route>
+            <Route path="/stock">
+              <StockPage/>
+            </Route>
+            <Route path="/">
+              <SearchPage />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
