02f9ced0093d894231e18ada241c9578f7f35e05
Peter Barry <peter@everve.com>
2020-03-29 19:17:57 +0100

    clean up header

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import SearchPage from './components/search/SearchPage';
 import StockPage from './components/stock/StockPage';
 import EditStock from './components/stock/EditStock';
 import ShopPage from './components/shop/ShopPage';
+import Navbar from './components/navbar/Navbar';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 import { Layout,Row, Col  } from 'antd';
-
+const { Content, Footer } = Layout;
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
+          <Navbar/>
+          <Content>
           <Row justify="center">
-            <Col xs={22} sm={12} >
+            <Col xs={22} sm={22} md={16} lg={12} >
               <Router>
                 <Switch>
                   <Route path="/shop">
@@ -38,6 +41,8 @@ export default class App extends Component {
               </Router>
             </Col>
           </Row>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>findstock.today ©2020 Created by Cell5 & friends</Footer>
         </Layout>
       </div>
     );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Logo from './Logo'
-import { Drawer, Button, Menu, Divider} from 'antd';
+import {Layout, Drawer, Button, Menu, Divider} from 'antd';
+const { Header } = Layout;
 class Navbar extends Component {
   state = {
     current: 'search',
@@ -18,21 +19,16 @@ onClose = () => {
   };
 render() {
     return (
-        <nav className="menuBar">
-          <Logo />
-          <div className="menuCon">
-            <div className="leftMenu">
-              <Menu mode="horizontal">
+        <Header>
+              <Logo />
+              <Menu mode="horizontal" theme="light">
                 <Menu.Item key="search">
                   <a href="/">Search</a>
                 </Menu.Item>
                 <Menu.Item key="about">
                   <a href="/about">About</a>
                 </Menu.Item>
-              </Menu>
-            </div>
-            <div className="rightMenu">
-              <Menu mode="horizontal">
+                <div class="spring"></div>
                 <Menu.Item key="signin">
                   <a href="/signin">Sign In</a>
                 </Menu.Item>
@@ -40,12 +36,11 @@ render() {
                   <a href="/signup">Sign Up</a>
                 </Menu.Item>
               </Menu>
-            </div>
-            <Button className="barsMenu" type="primary" onClick={this.showDrawer}>
-              <span className="barsBtn"></span>
-            </Button>
+              <Button className="barsMenu" type="primary" onClick={this.showDrawer}>
+                  <span className="barsBtn"></span>
+              </Button>
             <Drawer
-              title="findstock.now"
+              title="Menu"
               placement="right"
               closable={false}
               onClose={this.onClose}
@@ -67,8 +62,7 @@ render() {
                 </Menu.Item>
                 </Menu>
             </Drawer>
-</div>
-        </nav>
+      </Header>
     );
   }
 }
