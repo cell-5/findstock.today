94cccdb0d21d86a0fdfabc6909199fdd87a2802e
Peter Barry <peter@everve.com>
2020-03-29 13:49:56 +0100

    auth0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react'
 import './App.css';
-
 import SearchPage from './components/search/SearchPage';
 import StockPage from './components/stock/StockPage';
 import EditStock from './components/stock/EditStock';
 import ShopPage from './components/shop/ShopPage';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import { Layout,Row, Col  } from 'antd';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Loading from "./components/Loading";
+import NavBar from "./components/NavBar";
+import Profile from "./views/Profile";
+import { useAuth0 } from "./react-auth0-spa";
+import history from "./utils/history";
+import initFontAwesome from "./utils/initFontAwesome";
+initFontAwesome();
 
-export default class App extends Component {
-  render() {
+import { Layout, Row, Col } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
+const App = () => {
+  const { loading } = useAuth0();
+  if (loading) {
+    return <Loading />;
+  }
     return (
-      <div className="App">
+          <BrowserRouter>
         <Layout>
+          <Header>
+            <NavBar></NavBar>
+          </Header>
+          <Content>
           <Row justify="center">
             <Col xs={22} sm={12} >
-              <Router>
                 <Switch>
                   <Route path="/shop">
                     <ShopPage />
@@ -28,18 +39,17 @@ export default class App extends Component {
                   <Route exact path="/stock/:shopId">
                     <StockPage />
                   </Route>
-                  <Route exact path="/stock/:shopId/edit">
-                    <EditStock />
-                  </Route>
+                  <PrivateRoute exact path="/stock/:shopId/edit" component={EditStock} />
                   <Route path="/">
                     <SearchPage />
                   </Route>
                 </Switch>
-              </Router>
             </Col>
           </Row>
+          </Content>
+          <Footer>
+          </Footer>
         </Layout>
-      </div>
+        </BrowserRouter>
     );
   }
-}
