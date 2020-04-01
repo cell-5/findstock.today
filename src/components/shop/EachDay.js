de8784478e4b957903c4392729a9a01d898d01da
Joaquin Frago <gjfrago@gmail.com>
2020-04-01 15:58:34 +0800

    Removed unnecessary states on EachDay

--- src/components/shop/EachDay.js
+++ src/components/shop/EachDay.js
@@ -5,10 +5,8 @@ const { RangePicker } = TimePicker;
 const EachDay = ({ day = '', hoursOpen }) => {
 
     const [time, setTime] = useState();
-    const [timeStart, setTimeStart] = useState('');
-    const [timeEnd, setTimeEnd] = useState('');
     const [isOpen, setIsOpen] = useState(false);
-    const [toHoursOpen, setToHoursOpen] = useState({isOpen: isOpen, from: timeStart, to: timeEnd});
+    const [toHoursOpen, setToHoursOpen] = useState({isOpen: isOpen, from: '', to: ''});
 
     const handleIsOpenChange = (e) => {
         setIsOpen(e.target.checked);
@@ -21,8 +19,6 @@ const EachDay = ({ day = '', hoursOpen }) => {
             to: newTime[1].format('HH:mm'),
         });
         setTime(newTime); 
-        setTimeStart(newTime[0].format('HH:mm'));
-        setTimeEnd(newTime[1].format('HH:mm'));
     };
 
     useEffect(() => {
@@ -30,10 +26,13 @@ const EachDay = ({ day = '', hoursOpen }) => {
     }, [isOpen, toHoursOpen]);
 
     return (
-        <div style = {{margin: '0.25rem', width: '100%', display: 'flex', justifyContent: 'flex-start'}}>
-            <Checkbox onChange={handleIsOpenChange}>{day}</Checkbox>
-            <span style = {{marginRight: '0.25rem'}}>: </span>
-            <RangePicker format={'HH:mm'} onChange={handleTimeChange} value={time} />
+        <div style = {{margin: '0.25rem', width: '100%', display: 'flex', justifyContent: 'space-around'}}>
+            <div style = {{width: '40%', display: 'flex', justifyContent: 'flex-start', alignItems: 'center'}}>
+                <Checkbox onChange={handleIsOpenChange} checked={isOpen} >{day} :</Checkbox>
+            </div>
+            <div style = {{width: '100%', display: 'flex', justifyContent: 'flex-start'}}>
+                <RangePicker format={'HH:mm'} onChange={handleTimeChange} value={time} />
+            </div>
         </div>
     );
 }
